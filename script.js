98b128d20c960fc8f443ce16cab58d4c2b660ebc
sagarchityal4-star <sagarchityal4@gmail.com>
2026-03-10 08:24:29 -0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-function validateForm()
-{
+function validateForm(){
 
 var name=document.getElementById("name").value;
 var email=document.getElementById("email").value;
 var country=document.getElementById("country").value;
 var message=document.getElementById("message").value;
 
-if(name=="" || email=="" || country=="" || message=="")
-{
+if(name=="" || email=="" || country=="" || message==""){
 alert("Please fill all fields");
 return false;
 }
 
-alert("Your enquiry has been submitted successfully!");
+alert("Enquiry submitted successfully");
 return true;
 
 }
